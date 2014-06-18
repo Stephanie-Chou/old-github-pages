abcac1422677e621002df454f43320c7bc36a933
Stephanie-Chou <stephanie.chou@outlook.com>
2014-06-18 13:41:33 -0700

    nav php

---
+++ unit1_projects/navigation.php
@@ -0,0 +1,23 @@
+<nav>
+	<h4>Archive</h4>
+
+	<ul>
+		<p> Technical </p>
+		
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week1_technical_blog.html">Week One:Websites</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week2_technical.html">Week Two: Positioning</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week3_technical.html">Week Three: Why JavaScript</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week4_technical.html">Week Four: Arrays vs Hashes</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week5_technical.html">Week Five: Instance Everything</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week6_technical.html">Week Six:OOP</a></li>
+		
+		<p> Cultural </p>
+		
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week1_cultural.html">Week One: DBC</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week3_cultural.html">Week Three: Learning Style</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week4_cultural.html">Week Four: Tech Needs Women</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week5_cultural.html">Week Five: Peer Pairing</a></li>
+			<li> <a href="https://stephanie-chou.github.io/unit1_projects/week6_cultural.html">Week Six: Dangerous Stereotypes</a></li>
+		
+	</ul>
+</nav>
