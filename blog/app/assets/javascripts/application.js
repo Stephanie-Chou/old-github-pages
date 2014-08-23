5e89811b89c2430b8b9973f2d2df8578c2d6b7df
Stephanie Chou <stephanie.chou@outlook.com>
2014-08-23 12:46:39 -0500

    directories

--- blog/app/assets/javascripts/application.js
+++ blog/app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require tree .
 //= require jquery
 //= require jquery_ujs
 //= require underscore
