f730a940535ec6a4e62fb06730bff68b00aa2ba8
Stephanie-Chou <stephanie.chou@outlook.com>
2014-08-25 13:45:41 -0500

    click and get the indiv blog using one controller

--- blog/app/assets/javascripts/angular/controllers/PostIndexCtrl.js
+++ blog/app/assets/javascripts/angular/controllers/PostIndexCtrl.js
@@ -1,5 +1,6 @@
-blogApp.controller('PostIndexCtrl', ['$scope', '$location', '$http', function($scope, $location ,$http){
-    $scope.posts = [];
+blogApp.controller('PostIndexCtrl', ['$scope', '$location', '$http', '$routeParams', function($scope, $location ,$http, $routeParams){
+    $scope.posts = []
+    $scope.post_index = $routeParams.id -1
     $http({method:'GET', url: './posts.json'}).success(function(data){
       $scope.posts = data;
     })
